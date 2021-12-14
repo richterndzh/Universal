dd2d5eecb3ddbc17de49e5d5efb1a107a34fcc04
richterndzh <richterndzh@gmail.com>
2021-12-14 13:00:37 +0300

    create: added section & style bottomPages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var tabsItem = $(".tabs-item__block")
+  var tabsItem = $(".tabs-item__block") // Подключение табов
   var contentItem = $(".tabs-content__block")
 
   tabsItem.on("click", function(event){
@@ -21,11 +21,56 @@ $(document).ready(function() {
     $(activeContent).addClass('tabs-mobileContent__block--active');
     $(this).addClass('tabs-mobileItem__block--active');
   });
-
+  // Подключение мобильного меню
   var menuButton = document.querySelector(".navbar-button")
   menuButton.addEventListener('click', function () {
     document
     .querySelector(".navbar-menu")
     .classList.toggle('navbar-menu--visible');
   });
+  // Подключение закладок
+
+  var bookmarkButton = document.querySelector(".bottomPages-articles__block-data__bookmark")
+    bookmarkButton.addEventListener('click', function () {
+    document
+    .querySelector(".bottomPages-articles__block-data__bookmark")
+    .classList.toggle('bottomPages-articles__block-data__bookmark--red');
+  });
+
+  var bookmarkButton2 = document.querySelector(".bottomPages-articles__block-data__bookmark2")
+    bookmarkButton2.addEventListener('click', function () {
+    document
+    .querySelector(".bottomPages-articles__block-data__bookmark2")
+    .classList.toggle('bottomPages-articles__block-data__bookmark2--red');
+  });
+
+  var bookmarkButton3 = document.querySelector(".bottomPages-articles__block-data__bookmark3")
+    bookmarkButton3.addEventListener('click', function () {
+    document
+    .querySelector(".bottomPages-articles__block-data__bookmark3")
+    .classList.toggle('bottomPages-articles__block-data__bookmark3--red');
+  });
+
+  var bookmarkButton4 = document.querySelector(".bottomPages-articles__block-data__bookmark4")
+    bookmarkButton4.addEventListener('click', function () {
+    document
+    .querySelector(".bottomPages-articles__block-data__bookmark4")
+    .classList.toggle('bottomPages-articles__block-data__bookmark4--red');
+  });
+
+  var bookmarkButton5 = document.querySelector(".bottomPages-articles__block-data__bookmark5")
+    bookmarkButton5.addEventListener('click', function () {
+    document
+    .querySelector(".bottomPages-articles__block-data__bookmark5")
+    .classList.toggle('bottomPages-articles__block-data__bookmark5--red');
+  });
+
+  var bookmarkButton5 = document.querySelector(".bottomPages-articles__block-data__bookmark6")
+    bookmarkButton5.addEventListener('click', function () {
+    document
+    .querySelector(".bottomPages-articles__block-data__bookmark6")
+    .classList.toggle('bottomPages-articles__block-data__bookmark6--red');
+  });
+
 });
+
