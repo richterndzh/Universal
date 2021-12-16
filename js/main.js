3c4edd8192ee6dedd91b7f123fd9ed7f314f4231
richterndzh <richterndzh@gmail.com>
2021-12-16 16:38:33 +0300

    create: added modal

--- js/main.js
+++ js/main.js
@@ -24,7 +24,36 @@ $(document).ready(function() {
   effect: "fade",
 })
 
+var modalButton = $("[data-toggle=modal]");
+  var closemodalButton = $(".modal__close");
+  modalButton.on("click", openModal); 
+  closemodalButton.on("click", closeModal);
 
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible"); 
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible"); 
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
+   $(document).keydown(function (e) {
+    // ESCAPE key pressed
+    if (e.keyCode == 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+  });
+  
   var tabsItem = $(".tabs-item__block") // Подключение табов
   var contentItem = $(".tabs-content__block")
 
@@ -99,7 +128,7 @@ $(document).ready(function() {
   });
 
    //Обработка форм 
-      $('.form').each(function () {
+      $('.claim').each(function () {
         $(this).validate({
         errorClass: "invalid",
         rules: {
@@ -114,6 +143,38 @@ $(document).ready(function() {
           required: "Введите адрес электронной почты",
           email: "Ваш адрес электронной почты должен быть в формате name@domain.com.",
         },
+      },
+    });
+  });
+  $('.form').each(function () {
+        $(this).validate({
+        errorClass: "validation",
+        rules: {
+          name: {
+            required: true,
+            minlength: 2,
+          },
+          mail: {
+            required: true,
+            mail: true,
+          },
+          phone: {
+            minlength: 16,
+          },
+      },
+      
+        messages: {
+          name: {
+              required: "Введите ваше имя",
+              minlength: "Имя должно состоять минимум из двух букв.",
+            },
+          mail: {
+            required: "Введите адрес электронной почты",
+            mail: "Ваш адрес электронной почты должен быть в формате name@domain.com.",
+          },
+          phone: {
+            required: "Введите ваш номер телефона",
+          },
       },
     });
   });
